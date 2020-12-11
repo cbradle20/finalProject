5bdc50a4f6c24187ef1ecbf44ecd1536599fd797
brandonrichardson416 <70527027+brandonrichardson416@users.noreply.github.com>
2020-12-11 18:45:37 -0500

    script.js update
    script.js update

--- public/script.js
+++ public/script.js
@@ -11,3 +11,30 @@ function payeeConversion(payeesList) {
       return collection;
     }, [])
   }
+
+  function serverResponse(jsonResults) {
+    console.log('jsonResults', jsonResults);
+    sessionStorage.setItem('payeesList', JSON.stringify(jsonResults));
+    const reorganizedData = convertRestaurantsToCategories(jsonResults);
+    const options = makeYourOptionsObject(reorganizedData);
+    const chart = new CanvasJS.Chart('chartContainer', options);
+    chart.render();
+  }
+
+  document.body.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = $(e.target).serializeArray();
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((jResults) => jResults.json())
+      .then((jsonResults) => serverResponse(Results))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
