7ed863b26f1c0d4d0fe9fe6190dbfbc6b035d6f8
Colin Bradley <colinbradley68@gmail.com>
2020-12-11 18:35:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -38,3 +38,20 @@ app.route('/api')
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
+/* Links:
+export API endpoints links:
+(The amount of money spent is data point 12 over before the description)
+2015: https://data.princegeorgescountymd.gov/resource/bh8z-9wkk.json?agency=POLICE
+2016: https://data.princegeorgescountymd.gov/resource/csi4-9jzc.json?agency=POLICE
+2017: https://data.princegeorgescountymd.gov/resource/364y-gm2b.json?agency=POLICE
+2018: https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json?agency=POLICE
+2019: https://data.princegeorgescountymd.gov/resource/p32t-azw8.json?agency=POLICE
+*/
+
+
+
+
+
+
