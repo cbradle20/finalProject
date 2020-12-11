3581b6c5705d29615b83d1b0fd01ced62d64cf72
Colin Bradley <colinbradley68@gmail.com>
2020-12-11 18:26:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.route('/api')
   .post(async(req, res) => {
 
     console.log('POST request detected');
-    const data = await fetch('https://data.princegeorgescountymd.gov/api/views/INLINE/rows.json?accessType=DOWNLOAD');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/bh8z-9wkk.json?agency=POLICE');
     const json = await data.json();
     console.log('Fetch request data', req.body);
     res.json(json);
